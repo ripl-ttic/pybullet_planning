dd75022abdd78f267b52038684b22b6f7115c1e8
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-09-11 13:05:52 +0900

    regard joint<=-100 to be dummy circular joints

--- src/pybullet_planning/interfaces/robots/joint.py
+++ src/pybullet_planning/interfaces/robots/joint.py
@@ -129,7 +129,9 @@ def movable_from_joints(body, joints):
     return [movable_from_original[joint] for joint in joints]
 
 def is_circular(body, joint):
-    if joint < -1:  # dummy link
+    if joint <= -100:  # dummy circular link
+        return True
+    elif joint < -1:  # dummy non-circular link
         return False
     joint_info = get_joint_info(body, joint)
     if joint_info.jointType == p.JOINT_FIXED:
